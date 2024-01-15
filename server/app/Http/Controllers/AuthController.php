b18ab4fa491e4286c46b5f495d40cdfb7efbae7a
phanquangduong2002 <phanquangduong2002@gmail.com>
2024-01-15 12:44:03 +0700

    commit

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\URL;
 use App\Models\PasswordReset;
 use Carbon\Carbon;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use App\Mail\SendMail;
 
 class AuthController extends Controller
 {
@@ -157,45 +157,64 @@ class AuthController extends Controller
 
     public function forgetPassword(Request $request)
     {
+        // try {
+        //     $user =  User::where('email', $request->email)->get();
+
+        //     if (count($user) > 0) {
+
+        //         $token = Str::random(40);
+        //         $domain = URL::to('/');
+        //         $url = $domain . '/reset-password?token=' . $token;
+
+        //         $data['url'] = $url;
+        //         $data['email'] = $request['email'];
+        //         $data['title'] = "Password Reset";
+        //         $data['body'] = "Please click on below link to reset your password.";
+
+        //         Mail::send('forgetPasswordMail', ['data' => $data], function ($message) use ($data) {
+        //             $message->to($data['email'])->subject($data['title']);
+        //         });
+
+        //         $datetime = Carbon::now()->format('Y-m-d H:i:s');
+
+        //         PasswordReset::updateOrCreate(
+        //             ['email' => $request->email],
+        //             [
+        //                 'email' => $request->email,
+        //                 'token' => $token,
+        //                 'created_at' => $datetime
+        //             ]
+        //         );
+
+        //         return response()->json([
+        //             'status' => true,
+        //             'message' => 'Please check your mail to reset your password'
+        //         ], 200);
+        //     } else {
+        //         return response()->json([
+        //             'status' => false,
+        //             'message' => 'User not found'
+        //         ], 404);
+        //     }
+        // } catch (Exception $e) {
+        //     return response()->json([
+        //         'status' => false,
+        //         'message' => $e->getMessage()
+        //     ], 500);
+        // }
+
         try {
-            $user =  User::where('email', $request->email)->get();
+            $mailData = [
+                'title' => 'Mail from LMS Platform',
+                'body' => 'Please click on below link to reset your password.'
+            ];
 
-            if (count($user) > 0) {
+            Mail::to('phanquangduong2002@gmail.com')->send(new SendMail($mailData));
 
-                $token = Str::random(40);
-                $domain = URL::to('/');
-                $url = $domain . '/reset-password?token=' . $token;
-
-                $data['url'] = $url;
-                $data['email'] = $request['email'];
-                $data['title'] = "Password Reset";
-                $data['body'] = "Please click on below link to reset your password.";
-
-                Mail::send('forgetPasswordMail', ['data' => $data], function ($message) use ($data) {
-                    $message->to($data['email'])->subject($data['title']);
-                });
-
-                $datetime = Carbon::now()->format('Y-m-d H:i:s');
-
-                PasswordReset::updateOrCreate(
-                    ['email' => $request->email],
-                    [
-                        'email' => $request->email,
-                        'token' => $token,
-                        'created_at' => $datetime
-                    ]
-                );
-
-                return response()->json([
-                    'status' => true,
-                    'message' => 'Please check your mail to reset your password'
-                ], 200);
-            } else {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'User not found'
-                ], 404);
-            }
+            return response()->json([
+                'status' => true,
+                'message' => 'Please check your mail to reset your password'
+            ], 200);
         } catch (Exception $e) {
             return response()->json([
                 'status' => false,
